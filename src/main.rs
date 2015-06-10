0a81e63c5d7d0fa008132266da8f92d24c01e4a5
Robert J. Lambert III <rob@rlambert3.com>
2015-06-09 22:57:26 -0400

    Changed around the api call pulls directly from the show's page

--- src/main.rs
+++ src/main.rs
@@ -23,16 +23,13 @@ fn main() {
 
     let mut body = String::new();
     res.read_to_string(&mut body).unwrap();
-    get_shows(& body, & mut show_list);
+    get_shows(&body, &mut show_list);
 
-    // println!("shows: {:?}", show_list);
-    // let redisTest = store::fetch_an_integer();
-    // println!("int: {:?}", redisTest);
-
-    cache_shows(&show_list);
+    // cache_shows(&show_list);
+    get_episodes(1012);
 }
 
-fn get_shows(src: & str, shows: & mut Vec<show::Show>) {
+fn get_shows(src: &str, shows: &mut Vec<show::Show>) {
     let show_re = match Regex::new("<option value=\"([0-9]*)\">(.*)</option>") {
         Ok(show_re) => show_re,
         Err(err) => panic!("{}", err),
@@ -42,19 +39,52 @@ fn get_shows(src: & str, shows: & mut Vec<show::Show>) {
 
     for cap in caps {
         if cap.at(1).unwrap_or("") != "" {
-            let id: i32 = cap.at(1).unwrap().parse::<i32>().unwrap();
+            let id: i64 = cap.at(1).unwrap().parse::<i64>().unwrap();
             let n: &str = cap.at(2).unwrap_or("");
             let mut nm: String = String::new();
             nm.push_str(n);
-            // shows.push(show::Show{id: id, name: nm });
+            shows.push(show::Show{id: id, name: nm });
         }
     }
+}
+
+// Silicon Valley 1012
+fn get_episodes(id: i64) {
+    let mut client = Client::new();
+
+    // This is easier
+    let show_id = id.to_string();
+    let url = format!("https://eztv.ch/shows/{id}/",
+                      id=&show_id);
+
+    let mut res = client.get(&url)
+        .header(Connection::close())
+        .send().unwrap();
+
+    let mut body = String::new();
+    res.read_to_string(&mut body).unwrap();
+
+    let re = "<a href=\"(.*)\" title=\"(.*)\" alt=\"(.*)\" class=\"epinfo\">(.*)</a>";
+    let show_re = match Regex::new(re) {
+        Ok(show_re) => show_re,
+        Err(err) => panic!("{}", err),
+    };
+
+    let caps = show_re.captures_iter(&body);
+
+    for cap in caps {
+        // 0 full
+        // 1 url
+        // 2 title
+        // 3 alt
+        // 4 contents
+        println!("Episode: {}", cap.at(4).unwrap());
+    }
+    assert_eq!(res.status, hyper::Ok);
 }
 
 fn cache_shows(shows: & Vec<show::Show>) {
     for show in shows {
         println!("show {:?}", show);
     }
-    let mut s = show::Show::new("/tmp/show.db".to_string());
-    // store::do_something();
 }
--- src/show.rs
+++ src/show.rs
@@ -1,63 +1,7 @@
 #![allow(dead_code)]
 
-use sqlite3::database::{Database};
-use sqlite3::cursor::{Cursor};
-use sqlite3::types::ResultCode::{SQLITE_ROW};
-use sqlite3::{open};
-
 #[derive(Debug)]
 pub struct Show {
-    dbh: Database,
-    id: i64,
-    name: String,
-}
-
-impl Show {
-    pub fn new(path: String) -> Show {
-        let mut shows = match open(&path) {
-            Ok(dbh) => Show {
-                dbh: dbh,
-                id: -1,
-                name: "bleh".to_string(),
-            },
-            Err(err) => panic!("{:?}", err),
-        };
-        shows.initialize();
-        shows
-    }
-
-    fn exec_or_die(&mut self, sql: &str) {
-        match self.dbh.exec(sql) {
-            Ok(true)  => (),
-            Ok(false) => panic!("exec: {}", self.dbh.get_errmsg()),
-            Err(msg)  => panic!("exec: {:?}, {:?}\nIn sql: '{}'\n",
-                                msg, self.dbh.get_errmsg(), sql)
-        }
-    }
-
-    fn initialize(&mut self) {
-        self.exec_or_die("CREATE TABLE IF NOT EXISTS
-                          shows (id   INTEGER PRIMARY KEY,
-                                 name VARCHAR )");
-        self.exec_or_die("CREATE UNIQUE INDEX IF NOT EXISTS
-                          Shows_UniqueName ON shows(name)");
-        self.exec_or_die("BEGIN");
-    }
-
-    fn prepare_or_die<'a>(&'a self, sql: &str) -> Cursor<'a> {
-        match self.dbh.prepare(sql, &None) {
-            Ok(s)  => s,
-            Err(x) => panic!("sqlite error: {:?} ({:?})",
-                             self.dbh.get_errmsg(), x),
-        }
-    }
-
-    fn select_one<'a>(&'a mut self, sql: &str) -> Option<Cursor<'a>> {
-        let mut cursor = self.prepare_or_die(sql);
-        if cursor.step() == SQLITE_ROW {
-            Some(cursor)
-        } else {
-            None
-        }
-    }
+    pub id: i64,
+    pub name: String,
 }
